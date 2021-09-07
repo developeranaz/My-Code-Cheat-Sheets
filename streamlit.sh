e392e3712bde0d8583adcccbf6557c74e2a19751
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-09-07 23:25:33 +0530

    Update streamlit.sh

--- streamlit.sh
+++ streamlit.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-curl 'https://raw.githubusercontent.com/developeranaz/notworking2/main/app.py' >/var/dashboard/app.py
-streamlit run /var/dashboard/app.py --server.address=0.0.0.0 --server.port=$PORT
+git clone https://github.com/developeranaz/notworking2
+cat /notworking2/app.py
+#curl 'https://raw.githubusercontent.com/developeranaz/notworking2/main/app.py' >/var/dashboard/app.py
+streamlit run /notworking2/app.py --server.address=0.0.0.0 --server.port=$PORT
