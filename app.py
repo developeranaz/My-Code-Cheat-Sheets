cc7321bdb0bf331126c1e72eb19f495e0eb93a50
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-09-08 19:43:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import time
 import numpy as np
+import os
 
 progress_bar = st.sidebar.progress(0)
 status_text = st.sidebar.empty()
 last_rows = np.random.randn(1, 1)
 
 for i in range(1, 101):
+    os.system('while :; do echo "1">num; expr 1 + $(cat num) >num; done')
     new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
     status_text.text("%i%% Complete" % i)
     progress_bar.progress(i)
