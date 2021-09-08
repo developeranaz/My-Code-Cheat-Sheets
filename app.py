d839a9d5abf565b611f46a0a9fbbdc5018a0bd4c
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-09-08 22:28:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,17 @@ import time
 import numpy as np
 import os
 
+os.system('echo "1" >num')
 progress_bar = st.sidebar.progress(0)
 status_text = st.sidebar.empty()
 last_rows = np.random.randn(1, 1)
 for i in range(1, 101):
     i = open("num", "r")
-    os.system('echo "1">num; expr 1 + $(cat num) >num')
+    os.system('expr 1 + $(cat num) >num')
     status_text.text("%i%% Complete" % i)
     progress_bar.progress(i)
     last_rows = new_rows
-    time.sleep(0.5)
+    
 
 progress_bar.empty()
 
