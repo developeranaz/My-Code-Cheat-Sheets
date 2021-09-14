47321daf62e3ef69f070f6a06ceadbefb4046d82
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-09-14 06:43:12 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo "$PORT" >/PORT
 git clone https://github.com/developeranaz/notworking2
 cat /notworking2/app.py
 #curl 'https://raw.githubusercontent.com/developeranaz/notworking2/main/app.py' >/var/dashboard/app.py
+cat /notworking2/app.py | sed "s|THERANDOMPORT|$PORT|g"
 python3 /notworking2/app.py
