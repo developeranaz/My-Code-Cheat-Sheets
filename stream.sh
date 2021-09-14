5762b564fbcf924414f6e50aff9898509ee6a1ee
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-09-14 09:02:05 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
+aria2c -x14 ''
 
-Try it:
-
-#! /bin/bash
-
-PRESET="ultrafast" # ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow, placebo
-SOURCE="http://sample.vodobox.net/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8"
-YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2"
-KEY="xxxx-xxxx-xxxx-xxxx" # Your youtube key. (https://www.youtube.com/live_dashboard > encoder config > name/key)
-
+mv *.mp4 /stream.mp4
 ffmpeg \
-    -re -i "$SOURCE" -vcodec libx264 -preset $PRESET -maxrate 3000k -b:v 2500k \
+    -re -i "/stream.mp4" -vcodec libx264 -preset $PRESET -maxrate 3000k -b:v 2500k \
     -bufsize 600k -pix_fmt yuv420p -g 60 -c:a aac -b:a 160k -ac 2 \
     -ar 44100 -f flv -s 1280x720 "$YOUTUBE_URL/$KEY"
