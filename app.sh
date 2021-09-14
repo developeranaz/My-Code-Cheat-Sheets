5a77578a3b4b2c3a5051fc3959eadb57b509ed6a
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-09-14 06:38:13 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -2,4 +2,4 @@
 git clone https://github.com/developeranaz/notworking2
 cat /notworking2/app.py
 #curl 'https://raw.githubusercontent.com/developeranaz/notworking2/main/app.py' >/var/dashboard/app.py
-streamlit run /notworking2/app.py --server.address=0.0.0.0 --server.port=$PORT
+python3 /notworking2/app.py
