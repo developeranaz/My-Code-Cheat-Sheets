946a4134455d2ea8556ea192277a47834c3476e2
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-09-24 13:35:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-from
+from selenium import webdriver 
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+#chrome_options.add_argument("--disable-extensions")
+#chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--no-sandbox") # linux only
+chrome_options.add_argument("--headless")
+# chrome_options.headless = True # also works
+driver = webdriver.Chrome(options=chrome_options)
+start_url = "https://duckgo.com"
+driver.get(start_url)
+print(driver.page_source.encode("utf-8"))
+driver.quit()
+# b'<!DOCTYPE html><
