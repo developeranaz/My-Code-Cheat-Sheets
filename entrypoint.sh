02b2d27d41f8ba5c7c5b8bd2d5e9ffe2b9cd07c6
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-09-24 21:04:20 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl 'https://raw.githubusercontent.com/developeranaz/notworking2/main/app.py' >/app.py
 #cat /app.py | sed "s|THELIVEURI|$LIVEURL|g"
-python3 /app.py && rclone rcd --rc-serve 
+python3 /app.py && rclone rcd --rc-serve --rc-addr=0.0.0.0:$PORT
