dfa371b0eb2cd3477cf4de9982899e77e119b3f6
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-09-26 23:02:37 +0530

    Update c.py

--- c.py
+++ c.py
@@ -6,7 +6,7 @@ chrome_options = Options()
 #chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox") # linux only
 #chrome_options.add_argument("--headless")
-chrome_options.headless = True # also works
+#chrome_options.headless = True # also works
 driver = webdriver.Chrome(options=chrome_options)
 start_url = "https://booyah.live/channels/34884818"
 driver.get(start_url)
