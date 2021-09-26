c3d604e3b1c64165ba85c7442c9495cffdab224d
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-09-26 22:56:26 +0530

    Update b.py

--- b.py
+++ b.py
@@ -10,7 +10,7 @@ chrome_options.headless = True # also works
 driver = webdriver.Chrome(options=chrome_options)
 start_url = "https://booyah.live/channels/34884818"
 driver.get(start_url)
-time.sleep(12000)
+time.sleep(10)
 print(driver.page_source.encode("utf-8"))
 driver.quit()
 # b'<!DOCTYPE html><
