a8cc6fff2b036855bf88763da0d5fc83c4d41c57
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-10-01 09:51:44 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 curl 'https://raw.githubusercontent.com/developeranaz/notworking2/main/app.py' >/app.py
 #cat /app.py | sed "s|THELIVEURI|$LIVEURL|g"
+git clone 'https://github.com/developeranaz/notworking2'
 jupyter notebook --ip=0.0.0.0 --port=$PORT --NotebookApp.token='' --NotebookApp.password=''
-python3 /app.py && rclone rcd --rc-serve --rc-addr=0.0.0.0:$PORT
+#python3 /app.py && rclone rcd --rc-serve --rc-addr=0.0.0.0:$PORT
