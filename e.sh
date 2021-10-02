18c0dbfca470360323a6d7cf633b0b6a73a86c10
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-10-02 14:31:01 +0530

    Update e.sh

--- e.sh
+++ e.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -r /notworking2
 git clone 'https://github.com/developeranaz/notworking2'
 echo "$PORT" >/PORT
 jupyter notebook --ip=0.0.0.0 --port=$(cat /PORT) --NotebookApp.token='' --NotebookApp.password=''
