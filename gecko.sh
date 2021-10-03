e81c8227649fd5316caa1273a95a1a3fc8d261d2
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-10-03 10:48:35 +0530

    Create gecko.sh

---
+++ gecko.sh
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Firefox()
+
+browser.get('http://www.yahoo.com')
+assert 'Yahoo' in browser.title
+
+elem = browser.find_element(By.NAME, 'p')  # Find the search box
+elem.send_keys('seleniumhq' + Keys.RETURN)
+
+browser.quit()
