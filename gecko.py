4177be4f48329b380386b51607c43f94a893b220
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-10-05 11:36:56 +0530

    Update gecko.py

--- gecko.py
+++ gecko.py
@@ -2,10 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import time
 options = Options()
-options.headless = True
+#options.headless = True
 driver = webdriver.Firefox(options=options, executable_path=r'/usr/bin/geckodriver')
-driver.get("https://google.com")
+driver.get("https://booyah.live/channels/21828603?source=27")
 time.sleep(10)
+driver.find_element_by_css_selector('.btn-play > button:nth-child(1)').click()
 print(driver.page_source.encode("utf-8"))
 print ("Headless Firefox Initialized")
-driver.quit()
+#driver.quit()
