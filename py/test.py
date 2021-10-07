cc8b21fe7844e2c8c346072f953d0c0c1698a8c9
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-10-07 14:26:02 +0530

    Update test.py

--- py/test.py
+++ py/test.py
@@ -2,8 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import time
 options = Options()
-#options.headless = True
-driver = webdriver.Firefox(options=options, executable_path=r'E:\Downloads\geckodriver.exe')
+options.headless = True
+#driver = webdriver.Firefox(options=options, executable_path=r'E:\Downloads\geckodriver.exe')
+driver = webdriver.Firefox(options=options, executable_path=r'/usr/bin/geckodriver')
 driver.get("https://app.treasure.cloud/auth/signup?code=YzNkNGVmZDUtMjZiOS0xMWVjLWFmNzgtMWY5NWYyY2E3NDhhOmYwYTE0NjdhLTMyYzEtMTFlYi1iMWI4LTViYTQzMmY1ZjBkMA==")
 time.sleep(10)
 driver.find_element_by_css_selector('button.mat-focus-indicator:nth-child(3)').click()
