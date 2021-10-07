f5dab7ebc9e836ac9a7b158a73a7e916fc4044e0
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-10-07 20:02:03 +0530

    Update run.sh

--- py/test/run.sh
+++ py/test/run.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-while :
-do
-
-cat t.py |sed "s|THEMAILSAC|$THEMAIL|g"
-done
+#!/bin/bash
+gen1=$(cat /proc/sys/kernel/random/uuid |sed 's/-//g'| colrm 10)
+gen2='@mailsac.com'
+echo "$gen1$gen2" >therandommail
+therandommail=$(cat therandommail)
+cat org1.py |sed "s|therandommail|$therandommail|g" >exe1.py
+python3 exe1.py
+pkill geckodriver
+pkill firefox
+curl "https://mailsac.com/inbox/$therandommail" >mailhtml
+therandomurl=$(cat mailhtml |sed 's/"/\n/g' |grep hubspot |head -1)
+cat org2.py |sed "s|therandomurl|$therandomurl|g" >exe2.py
+python3 exe2.py
+pkill geckodriver
+pkill firefox
