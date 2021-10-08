8bc5f492a7179d2af4541f86131cad2b4626d108
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-10-08 19:53:49 +0530

    Update run.sh

--- py/test/run.sh
+++ py/test/run.sh
@@ -8,8 +8,11 @@ cat org1.py |sed "s|therandommail|$therandommail|g" >exe1.py
 python3 exe1.py
 pkill geckodriver
 pkill firefox
+echo "sleep 60"
+sleep 60
 curl "https://mailsac.com/inbox/$therandommail" >mailhtml
 therandomurl=$(cat mailhtml |sed 's/"/\n/g' |grep hubspot |head -1)
+echo $therandomurl >url
 cat org2.py |sed "s|therandomurl|$therandomurl|g" >exe2.py
 python3 exe2.py
 pkill geckodriver
