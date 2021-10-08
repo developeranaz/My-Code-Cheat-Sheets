ac4d45bca2fd88f5444f9504e1af7a7a5cd4d45b
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-10-08 17:27:18 +0530

    Update g1.py

--- sdfbxf/g1.py
+++ sdfbxf/g1.py
@@ -2,9 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import time
 options = Options()
-#options.headless = True
-driver = webdriver.Firefox(options=options, executable_path=r'E:\Downloads\geckodriver.exe')
-#driver = webdriver.Firefox(options=options, executable_path=r'/usr/bin/geckodriver')
+options.headless = True
+#driver = webdriver.Firefox(options=options, executable_path=r'E:\Downloads\geckodriver.exe')
+driver = webdriver.Firefox(options=options, executable_path=r'/usr/bin/geckodriver')
 driver.get("https://app.treasure.cloud/auth/signup?code=YzNkNGVmZDUtMjZiOS0xMWVjLWFmNzgtMWY5NWYyY2E3NDhhOmYwYTE0NjdhLTMyYzEtMTFlYi1iMWI4LTViYTQzMmY1ZjBkMA==")
 time.sleep(10)
 driver.find_element_by_css_selector('button.mat-focus-indicator:nth-child(3)').click()
@@ -14,4 +14,5 @@ driver.find_element_by_css_selector('#mat-input-1').send_keys("kolpj875@mailto.p
 driver.find_element_by_css_selector('#mat-input-2').send_keys("Xhzoamxtqyei1@gmail.com")
 driver.find_element_by_css_selector('#mat-input-3').send_keys("Xhzoamxtqyei1@gmail.com")
 driver.find_element_by_css_selector('#continue-button').click()
-#driver.quit()
+time.sleep(5)
+driver.quit()
