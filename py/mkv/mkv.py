ed5e93c36bdcf08a1ad2a3619540c3eb32ce7ab1
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-10-09 23:07:31 +0530

    Update mkv.py

--- py/mkv/mkv.py
+++ py/mkv/mkv.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import time
 options = Options()
-#options.headless = True
+options.headless = True
 driver = webdriver.Firefox(options=options, executable_path=r'/usr/bin/geckodriver')
 driver.get("https://mkvking.me/injustice-2021/")
 time.sleep(1)
@@ -10,4 +10,4 @@ driver.find_element_by_css_selector('#download > ul > li:nth-child(2) > a').clic
 time.sleep(10)
 print(driver.page_source.encode("utf-8"))
 print ("Headless Firefox Initialized")
-#driver.quit()
+driver.quit()
