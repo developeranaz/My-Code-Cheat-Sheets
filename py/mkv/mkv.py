9b783cf702bfb2d928b733b0910fb70540ac3699
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-10-11 23:52:55 +0530

    Update mkv.py

--- py/mkv/mkv.py
+++ py/mkv/mkv.py
@@ -11,6 +11,17 @@ time.sleep(15)
 driver.find_ > a').click()
 time.sleep(15)
 
+        driver = self.driver
+        driver.get("https://mkvking.me/the-night-house-2020/")
+        driver.find_element_by_link_text("GoogleDrive 720p").click()
+        driver.get("https://tribuntekno.com/?id=SGdLN1RMS05YSUJwcDJSVlg0d09RdDd3YlYvSDV5RkdvWHBwZlBEcnpESVRCall1cmFvWmZQOStXTi9qY2FkbXRIcTZZZ21sSVNJRHkzU1BHa1lKM1YwYjE0YnEyN3NzVUdWaDJJa0tuMjA9")
+        time.sleep(15)
+        driver.find_element_by_xpath("//img[contains(@src,'https://tribuntekno.com/wp-content/uploads/button_im-not-a-robot.png')]").click()
+        time.sleep(15)
+        driver.find_element_by_xpath("//img[contains(@src,'https://tribuntekno.com/wp-content/uploads/button_generate-link.png')]").click()
+        time.sleep(15)
+        driver.find_element_by_id("showlink").click()
+
 print(driver.page_source.encode("utf-8"))
 print ("Headless Firefox Initialized")
 driver.quit()
