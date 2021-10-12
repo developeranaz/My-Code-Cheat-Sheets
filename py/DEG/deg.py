6ce6c682ef667bb0ff568e2179cacdcdd738f5bf
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-10-12 17:10:29 +0530

    Update deg.py

--- py/DEG/deg.py
+++ py/DEG/deg.py
@@ -1,9 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import time
+import os
+options = Options()
+#options.headless = True
+driver = webdriver.Firefox(options=options, executable_path=r'E:\Downloads\geckodriver.exe')
+#driver = webdriver.Firefox(options=options, executable_path=r'/usr/bin/geckodriver')
 driver.get("https://degoo.com/")
-        driver.find_element_by_id("Email").click()
-        driver.find_element_by_id("Email").clear()
-        driver.find_element_by_id("Email").send_keys("bodhamkk@gmail.com")
-        driver.find_element_by_id("Password").click()
-        driver.find_element_by_id("Password").clear()
-        driver.find_element_by_id("Password").send_keys("suuuuuuuuuu1SX")
-        driver.find_element_by_id("submit-btn-ff734fd2-dd44-4b79-a7ac-2409605aecf3").click()
-        driver.find_element_by_link_text("Choose this plan").click()
+time.sleep(2)
+driver.find_element_by_css_selector('#accept-cookies').click()
+time.sleep(2)
+driver.find_element_by_css_selector('#Email').send_keys("kolpj875@mailto.plus")
+driver.find_element_by_css_selector('#Password').send_keys("kolpj875@mailto.plus")
+fullhtmlfile = driver.page_source.encode("utf-8")
+print(fullhtmlfile, file=open("output.html", "a"))
+os.system("degoobtnfinder -h output.html >thenewbtn")
+#do something to make it css selector
+
+thechangingbtn = open('thenewbtn', 'r').read()
+driver.find_element_by_css_selector(thechangingbtn).click()
+driver.quit()
