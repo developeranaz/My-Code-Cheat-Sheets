d168d9fd1913452f2c3ec9310b312fc5f1baf1ad
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-10-12 17:10:54 +0530

    Update deg.py

--- py/DEG/deg.py
+++ py/DEG/deg.py
@@ -3,9 +3,9 @@ from selenium.webdriver.firefox.options import Options
 import time
 import os
 options = Options()
-#options.headless = True
-driver = webdriver.Firefox(options=options, executable_path=r'E:\Downloads\geckodriver.exe')
-#driver = webdriver.Firefox(options=options, executable_path=r'/usr/bin/geckodriver')
+options.headless = True
+#driver = webdriver.Firefox(options=options, executable_path=r'E:\Downloads\geckodriver.exe')
+driver = webdriver.Firefox(options=options, executable_path=r'/usr/bin/geckodriver')
 driver.get("https://degoo.com/")
 time.sleep(2)
 driver.find_element_by_css_selector('#accept-cookies').click()
