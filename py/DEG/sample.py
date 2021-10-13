512c67c308c0cbfa3af12f40aae22a12faad4d83
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-10-13 08:10:02 +0530

    Update sample.py

--- py/DEG/sample.py
+++ py/DEG/sample.py
@@ -1,56 +1,25 @@
-# -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+from selenium.webdriver.firefox.options import Options
+import time
+import os
+options = Options()
+options.headless = True
+#driver = webdriver.Firefox(options=options, executable_path=r'E:\Downloads\geckodriver.exe')
+driver = webdriver.Firefox(options=options, executable_path=r'/usr/bin/geckodriver')
+driver.get("https://degoo.com/")
+time.sleep(2)
+driver.find_element_by_css_selector('#accept-cookies').click()
+time.sleep(2)
+driver.find_element_by_css_selector('#Email').send_keys("kolpj875@mailto.plus")
+driver.find_element_by_css_selector('#Password').send_keys("kolpj875@mailto.plus")
+fullhtmlfile = driver.page_source.encode("utf-8")
+print(fullhtmlfile, file=open("/output.html", "a"))
+os.system("degoobtnfinder")
+thechangingbtn = open('/thenewbtn', 'r').read()
+print(thechangingbtn)
+driver.find_element_by_css_selector(thechangingbtn).click()
+time.sleep(10)
+driver.find_element_by_css_selector(".product-free > div:nth-child(12) > div:nth-child(1) > a:nth-child(1)").click()
 
-class UntitledTestCase(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
-    
-    def test_untitled_test_case(self):
-        driver = self.driver
-        driver.get("https://degoo.com/")
-        driver.find_element_by_id("Email").click()
-        driver.find_element_by_id("Email").clear()
-        driver.find_element_by_id("Email").send_keys("bodhamkk@gmail.com")
-        driver.find_element_by_id("Password").click()
-        driver.find_element_by_id("Password").clear()
-        driver.find_element_by_id("Password").send_keys("suuuuuuuuuu1SX")
-        driver.find_element_by_id("submit-btn-ff734fd2-dd44-4b79-a7ac-2409605aecf3").click()
-        driver.find_element_by_link_text("Choose this plan").click()
-    
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
-    def tearDown(self):
-        self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
 
-if __name__ == "__main__":
-    unittest.main()
+time.sleep(10)
