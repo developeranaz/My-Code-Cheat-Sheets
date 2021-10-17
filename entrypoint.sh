9c847cb8e0aa208183e31ff04f7079adfbd76cb8
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-10-17 17:30:00 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,3 +2,4 @@
 #git clone "https://github.com/developeranaz/notworking2"
 echo "$PORT" >/PORT
 cat /default |sed "s|therandomport|$(cat /PORT)|g" >/etc/apache2/sites-enabled/000-default.conf
+/usr/bin/supervisord
