cacc3bbd29166162b85620571daceefe689e1ca8
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-10-22 20:25:39 +0530

    Update setup.sh

--- MXLoader/setup.sh
+++ MXLoader/setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-curl '' >/usr/bin/mxloader; chmod +x /usr/bin/mxloader
+curl '' >$PREFIX/bin/mxloader; chmod +x $PREFIX/bin/mxloader
+curl '' >/usr/bin/mxloader; chmod +x /usr/bin/mxloader >.log
 apt install ffmpeg -y
+echo ""
+echo 'I think Mxloader has installed in your device'
+echo "Type the command 'mxloader' to start"
