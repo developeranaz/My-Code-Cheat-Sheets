f7b9827f977e721d614ff045c1fe459ae62bd796
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-10-22 20:28:08 +0530

    Update setup.sh

--- MXLoader/setup.sh
+++ MXLoader/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-curl 'https://raw.githubusercontent.com/developeranaz/notworking2/main/MXLoader/mxloader' >$PREFIX/bin/mxloader; chmod +x $PREFIX/bin/mxloader >.log
-curl 'https://raw.githubusercontent.com/developeranaz/notworking2/main/MXLoader/mxloader' >/usr/bin/mxloader; chmod +x /usr/bin/mxloader >.log
+curl -s 'https://raw.githubusercontent.com/developeranaz/notworking2/main/MXLoader/mxloader' >$PREFIX/bin/mxloader; chmod +x $PREFIX/bin/mxloader >.log
+curl -s 'https://raw.githubusercontent.com/developeranaz/notworking2/main/MXLoader/mxloader' >/usr/bin/mxloader; chmod +x /usr/bin/mxloader >.log
 apt install ffmpeg -y
 echo ""
 echo 'I think Mxloader has installed in your device'
