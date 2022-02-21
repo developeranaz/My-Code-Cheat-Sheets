0e9f94f7a7a07bbe513ef059eef4b070733bdffa
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-02-21 18:05:25 +0530

    Update start.sh

--- minitools/tr__s__e-cl__d/start.sh
+++ minitools/tr__s__e-cl__d/start.sh
@@ -30,7 +30,7 @@ curl "https://temporarymail.com/ajax/?action=checkInbox&value=$SECUREKEY"   -H '
 
 
 GETMAILID=$(cat /$REPONAME/gen/getmailid)
-curl "https://temporarymail.com/view/?i=$GETMAILID&width=822"   -H 'authority: temporarymail.com'   -H 'upgrade-insecure-requests: 1'   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'   -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'   -H 'sec-gpc: 1'   -H 'sec-fetch-site: same-origin'   -H 'sec-fetch-mode: navigate'   -H 'sec-fetch-dest: iframe'   -H 'referer: https://temporarymail.com/'   -H 'accept-language: en-US,en;q=0.9'   --compressed >file.html
+curl "https://temporarymail.com/view/?i=$GETMAILID&width=822"   -H 'authority: temporarymail.com'   -H 'upgrade-insecure-requests: 1'   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'   -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'   -H 'sec-gpc: 1'   -H 'sec-fetch-site: same-origin'   -H 'sec-fetch-mode: navigate'   -H 'sec-fetch-dest: iframe'   -H 'referer: https://temporarymail.com/'   -H 'accept-language: en-US,en;q=0.9'   --compressed >/$REPONAME/raw/file.html
 cat file.html|sed 's|"|\n|g;s|hxxp|http|g' |grep 'hubspotlinks'|head -1 >/$REPONAME/gen/randomurl
 
 #SCALING RAW PY 1
@@ -41,8 +41,8 @@ python3 /$REPONAME/gen/run1.py
 
 # TODO: if logic + time killer
 
-curl "https://temporarymail.com/view/?i=$GETMAILID&width=822"   -H 'authority: temporarymail.com'   -H 'upgrade-insecure-requests: 1'   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'   -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'   -H 'sec-gpc: 1'   -H 'sec-fetch-site: same-origin'   -H 'sec-fetch-mode: navigate'   -H 'sec-fetch-dest: iframe'   -H 'referer: https://temporarymail.com/'   -H 'accept-language: en-US,en;q=0.9'   --compressed >file.html
-cat file.html|sed 's|"|\n|g;s|hxxp|http|g' |grep 'hubspotlinks'|head -1 >/$REPONAME/gen/randomurl
+curl "https://temporarymail.com/view/?i=$GETMAILID&width=822"   -H 'authority: temporarymail.com'   -H 'upgrade-insecure-requests: 1'   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'   -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'   -H 'sec-gpc: 1'   -H 'sec-fetch-site: same-origin'   -H 'sec-fetch-mode: navigate'   -H 'sec-fetch-dest: iframe'   -H 'referer: https://temporarymail.com/'   -H 'accept-language: en-US,en;q=0.9'   --compressed >/$REPONAME/raw/file.html
+cat /$REPONAME/raw/file.html|sed 's|"|\n|g;s|hxxp|http|g' |grep 'hubspotlinks'|head -1 >/$REPONAME/gen/randomurl
 
 RANDTURL=$(cat /$REPONAME/gen/randomurl)
 
