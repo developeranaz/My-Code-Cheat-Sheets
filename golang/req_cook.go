5ec9c93e5f32799f2150427b8200fb4870883585
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-03-15 19:35:25 +0530

    Update req_cook.go

--- golang/req_cook.go
+++ golang/req_cook.go
@@ -1,1 +1,46 @@
+package main
 
+import (
+  "fmt"
+  "strings"
+  "net/http"
+  "io/ioutil"
+)
+
+func main() {
+
+  url := "http://localhost:8080/api/v2/auth/login"
+  method := "POST"
+
+  payload := strings.NewReader(`username=admin&password=adminadmin`)
+
+  client := &http.Client {
+  }
+  req, err := http.NewRequest(method, url, payload)
+
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  req.Header.Add("Referer", "http://localhost:8080/")
+  req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36")
+  req.Header.Add("Content-type", "application/x-www-form-urlencoded; charset=UTF-8")
+
+  res, err := client.Do(req)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  defer res.Body.Close()
+
+
+  cookies := res.Cookies()
+  fmt.Println(cookies)
+
+  body, err := ioutil.ReadAll(res.Body)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  fmt.Println(string(body))
+}
