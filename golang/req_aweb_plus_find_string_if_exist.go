4c5ff67eec78bb6b4b94aef16bd981b624c9113c
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-03-23 22:13:28 +0530

    Update req_aweb_plus_find_string_if_exist.go

--- golang/req_aweb_plus_find_string_if_exist.go
+++ golang/req_aweb_plus_find_string_if_exist.go
@@ -1,29 +1,34 @@
 package main
 
 import (
-        "fmt"                                                      "io/ioutil"
-        "net/http"
-        "strings"
-        "time"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
 )
-                                                           func main() {
 
-        for {
-                c := http.Client{Timeout: time.Duration(1) * time.Second}                                                             resp, err := c.Get("http://127.0.0.1:8080")
-                if err != nil {
-                        fmt.Printf("Error %s", err)
-                        return
-                }
-                defer resp.Body.Close()
-                body, err := ioutil.ReadAll(resp.Body)
-                responseString := string(body)
-                substr := "usery"
-                if strings.Contains(responseString, substr) {                                                                                 fmt.Println("The substring is present in the string.")
-                } else {
-                        fmt.Println("The substring is not present in the string.")
-                }                                          
-        }
-        //    fmt.Printf("Body : %s", body)
+func main() {
 
-        //strings.Contains(body ,"user")
+	for {
+		c := http.Client{Timeout: time.Duration(1) * time.Second}
+		resp, err := c.Get("http://127.0.0.1:8080")
+		if err != nil {
+			fmt.Printf("Error %s", err)
+			return
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		responseString := string(body)
+		substr := "usery"
+		if strings.Contains(responseString, substr) {
+			fmt.Println("The substring is present in the string.")
+		} else {
+			fmt.Println("The substring is not present in the string.")
+		}
+
+	}
+	//    fmt.Printf("Body : %s", body)
+
+	//strings.Contains(body ,"user")
 }
