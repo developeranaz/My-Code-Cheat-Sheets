b895f8621d135fde4e3865ca11680edc49e22510
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-04-05 15:36:05 +0530

    Create gen.sh

---
+++ golang/go_qb/bak/gen.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+cat bak.raw >raw
+echo >new
+while :
+do
+first=$(cat raw |head -1)
+echo $first
+firstname=$(echo $first |sed 's/"//g;s/:/\n/g' |head -1)
+lastname=$(echo $first |sed 's/"//g;s/:/\n/g' |tail -1)
+#echo $lastname
+
+if test -z "$first"
+then
+      echo "empty first"
+      exit
+else
+      echo "not"
+fi
+
+
+
+if test -z "$lastname"
+then
+      echo "is empty"
+      lastname="emptyu"
+      echo "$lastname"
+else
+      echo "is NOT empty"
+fi
+wu=$(echo $first |sed "s@$lastname@ + *qb_$firstname + @g")
+echo $wu >> new
+tail -n +2 raw >.raw
+cat .raw >raw
+#echo "$lastname"
+done
