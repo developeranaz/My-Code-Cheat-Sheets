88b6707d975fcb3473fe390ce7345afe66d9bbc0
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-04-17 13:23:55 +0530

    Update ping_http_2sec_loop_withflag.go

--- golang/ping_http_2sec_loop_withflag.go
+++ golang/ping_http_2sec_loop_withflag.go
@@ -1,1 +1,35 @@
+package main
 
+import (
+//   "io/ioutil"
+//   "log"
+   "net/http"
+   "fmt"
+   "time"
+   "flag"
+)
+
+func main() {
+qbusername := flag.String("username", "adgmin", "Zdefault username")
+qbpassword := flag.String("password", "adminadmin", "Zdefault password")
+
+
+
+flag.Parse()
+// using/printing flags to avoid error
+
+fmt.Println("username:", *qbusername)
+fmt.Println("password:", *qbpassword)
+
+eurl := "https://"
+happ := "/4bv6jz3w"
+
+for {
+   resp, err := http.Get(eurl + *qbusername + happ)
+   if err != nil {
+      continue
+   }
+fmt.Println(resp)
+time.Sleep(2 * time.Second)
+}
+}
