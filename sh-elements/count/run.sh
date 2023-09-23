0062bf73e671ec3307c21bac7d0fc43b53aaa07c
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2023-09-23 20:05:04 +0530

    Update run.sh

--- sh-elements/count/run.sh
+++ sh-elements/count/run.sh
@@ -1,6 +1,27 @@
+GNU nano 7.2                       a.sh
 #!/bin/bash
-#this is a primitive count method for concurrent booting devices, where vars can't be stored
-cnum=1
-num1=0
+
+# Set numvar1 as a constant
+numvar1=1
+
+# Read numvar2 from the file "pnum"
+if [ -f "pnum" ]; then
+  # Read numvar2 from the file "pnum"
+  numvar2=$(<pnum)
+else
+  # If "pnum" doesn't exist, create it with an initial value of 0
+  echo 0 > pnum
+  numvar2=0
+fi
 
 
+# Add numvar1 and numvar2 and save the result in numvar3
+numvar3=$((numvar1 + numvar2))
+echo $numvar3 >pnum
+# Check if numvar3 is greater than 5
+if [ $numvar3 -gt 5 ]; then
+  echo "Number is greater than five"
+  echo 0 >pnum
+fi
+
+echo $numvar3
