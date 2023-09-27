7ea69febdb9d9d5a1970fa54148793705406df3d
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2023-09-27 06:48:20 +0530

    Update run.sh

--- minitools/MS-Auto-Reward/run.sh
+++ minitools/MS-Auto-Reward/run.sh
@@ -1,52 +1,42 @@
 #!/bin/bash
 
 while true; do
+  input="$(./ip.sh)"
+  comparison="$(cat ip)"
 
+  echo $input
+  echo $comparison
 
-input="$(./ip.sh)"
-comparison="$(cat ip)"
-
-
-echo $input
-echo $comparison
-
-
-if [ "$input" = "$comparison" ]; then
-   while true; do
-    echo "Manual IP Change Required."
-#    mpv Snapchat.mp3
-    sleep 3
-  break
-   done
-else
+  if [ "$input" = "$comparison" ]; then
+    while true; do
+      echo "Manual IP Change Required."
+      # mpv Snapchat.mp3
+      sleep 3
+      break
+    done
+  else
     echo "IP Changed Proceeding to Next Profile:)"
-    ./ip.sh >ip
-
-#ffxxxxxxxxxxxxx
+    ./ip.sh > ip
 
 
-cnum=1
+    cnum=1
 
-while :
-do
-initnum=$(cat pnum)
-pnum=$((cnum + initnum))
-echo $pnum
-echo $pnum >pnum
+    while :
+    do
+      initnum=$(cat pnum)
+      pnum=$((cnum + initnum))
+      echo $pnum
+      echo $pnum > pnum
 
-./auto.sh -p$pnum
+      ./auto.sh -p$pnum
 
-if [ "$pnum" -gt 16 ]; then
-echo 0 >pnum
+      if [ "$pnum" -gt 16 ]; then
+        echo 0 > pnum
+        break
+      fi
+      break
+    done
 
-break
-fi
-break
-done
-
-
-#ffxxxxxxxxxx
-
-fi
+  fi
 
 done
