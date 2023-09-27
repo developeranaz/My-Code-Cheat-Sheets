6ad0024e6157db1ac21802000751ab9a6dd999ee
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2023-09-27 07:35:10 +0530

    Create install.sh

---
+++ minitools/MS-Auto-Reward/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl 'https://raw.githubusercontent.com/developeranaz/My-Code-Cheat-Sheets/main/minitools/MS-Auto-Reward/auto.sh' >./auto.sh
+curl 'https://raw.githubusercontent.com/developeranaz/My-Code-Cheat-Sheets/main/minitools/MS-Auto-Reward/run.sh' >./run.sh
+curl 'https://raw.githubusercontent.com/developeranaz/My-Code-Cheat-Sheets/main/minitools/MS-Auto-Reward/ip.sh' >./ip.sh
+
+
